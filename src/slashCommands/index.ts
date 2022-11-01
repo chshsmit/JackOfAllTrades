fa0281b1c84fed4c16cb5f46ecb75fa39ae5bea8
Christopher Smith <chshsmit@gmail.com>
2022-10-31 20:25:13 -0600

    Removing another command

--- src/slashCommands/index.ts
+++ src/slashCommands/index.ts
@@ -1,6 +1,5 @@
 import Command from "../interfaces/Command";
 import Bangerz from "./Bangerz";
-import Ping from "./Ping";
 import RollDice from "./RollDice";
 
-export const COMMANDS: Command[] = [new Ping(), new RollDice(), new Bangerz()];
+export const COMMANDS: Command[] = [new RollDice(), new Bangerz()];
