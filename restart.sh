56ac6e185b102a850417d6a48abae9418a1006c2
Christopher Smith <chshsmit@gmail.com>
2022-11-26 17:47:49 -0700

    Using pm2 instead of Docker

--- restart.sh
+++ restart.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-docker build -t chshsmit/jackofalltrades .
-docker stop jack_of_all_trades
-docker rm jack_of_all_trades
-docker image prune --filter="dangling=true" -f
-docker run --name jack_of_all_trades -d chshsmit/jackofalltrades
+# docker build -t chshsmit/jackofalltrades .
+# docker stop jack_of_all_trades
+# docker rm jack_of_all_trades
+# docker image prune --filter="dangling=true" -f
+# docker run --name jack_of_all_trades -d chshsmit/jackofalltrades
+
+
+pm2 kill
+npx tsc
+STAGE=PROD pm2 start .build/src/Bot.js
