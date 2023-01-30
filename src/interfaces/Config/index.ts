0f12626ed81f0cf338ea9b01b2111d559d5e5ff3
Christopher Smith <chshsmit@gmail.com>
2023-01-29 17:50:16 -0700

    Added support for spotify

--- src/interfaces/Config/index.ts
+++ src/interfaces/Config/index.ts
@@ -2,6 +2,11 @@ interface Config {
   discordToken: string;
   guildId: string;
   applicationId: string;
+
+  spotifyApiSecret: string;
+  spotifyClientId: string;
+  spotifyPlaylistId: string;
+  spotifyRefreshToken: string;
 }
 
 export default Config;
--- src/slashCommands/Bangerz/index.ts
+++ src/slashCommands/Bangerz/index.ts
@@ -1,12 +1,14 @@
+import axios from "axios";
 import {
   CacheType,
   ChatInputCommandInteraction,
   EmbedBuilder,
   SlashCommandBuilder,
 } from "discord.js";
+import getVideoId from "get-video-id";
 import GoogleApiController from "../../api/GoogleApiController";
 import Command from "../../interfaces/Command";
-import getVideoId from "get-video-id";
+import { getConfiguration } from "../../util/util";
 
 export default class Bangerz implements Command {
   data: SlashCommandBuilder | any;
@@ -14,11 +16,11 @@ export default class Bangerz implements Command {
   constructor() {
     this.data = new SlashCommandBuilder()
       .setName("banger")
-      .setDescription("Add a song to the google sheet")
+      .setDescription("Add a song to the google sheet or spotify playlist")
       .addStringOption((option) =>
         option
           .setName("url")
-          .setDescription("The url to the song on youtube")
+          .setDescription("The url to the song on youtube or spotify")
           .setRequired(true)
       );
   }
@@ -26,24 +28,100 @@ export default class Bangerz implements Command {
   async execute(
     interaction: ChatInputCommandInteraction<CacheType>
   ): Promise<void> {
-    const youtubeUrl = interaction.options.getString("url")!;
-    const { id } = getVideoId(youtubeUrl);
+    await interaction.reply("Woking on it...");
 
+    const songUrl = interaction.options.getString("url")!;
+    if (songUrl.includes("spotify")) {
+      this.handleSpotifyUrl(songUrl, interaction);
+    } else {
+      this.handleYoutubeUrl(songUrl, interaction);
+    }
+  }
+
+  private async handleSpotifyUrl(
+    url: string,
+    interaction: ChatInputCommandInteraction<CacheType>
+  ): Promise<void> {
+    const config = getConfiguration();
+
+    const idSubstring = url.split("track/")[1];
+    const songId = idSubstring.split("?")[0];
+
+    const spotifyAuth = await axios({
+      method: "POST",
+      url: `https://accounts.spotify.com/api/token?grant_type=refresh_token&refresh_token=${config.spotifyRefreshToken}`,
+      headers: {
+        Authorization:
+          "Basic " +
+          Buffer.from(
+            config.spotifyClientId + ":" + config.spotifyApiSecret
+          ).toString("base64"),
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    });
+
+    const addSongToPlaylist = await axios.post(
+      `https://api.spotify.com/v1/playlists/${config.spotifyPlaylistId}/tracks`,
+      {
+        uris: [`spotify:track:${songId}`],
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${spotifyAuth.data.access_token}`,
+        },
+      }
+    );
+
+    if (addSongToPlaylist.data.snapshot_id) {
+      const songData = await axios.get(
+        `https://api.spotify.com/v1/tracks/${songId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${spotifyAuth.data.access_token}`,
+          },
+        }
+      );
+
+      const artists = songData.data.artists.map((artist: any) => {
+        return artist.name;
+      });
+
+      const response = new EmbedBuilder()
+        .setTitle("Added a song!")
+        .setDescription(
+          `I added ${songData.data.name} by ${artists.join(
+            ", "
+          )} to the playlist!`
+        );
+
+      await interaction.followUp({ embeds: [response] });
+    } else {
+      await interaction.followUp("Something went wrong");
+    }
+  }
+
+  private async handleYoutubeUrl(
+    url: string,
+    interaction: ChatInputCommandInteraction<CacheType>
+  ): Promise<void> {
+    const { id } = getVideoId(url);
     if (id === null) {
-      await interaction.reply("Sorry that is an invalid video.");
+      await interaction.followUp("Sorry that is an invalid video.");
     } else {
       const api = new GoogleApiController();
       const videoTitle = await api.getVideoTitle(id);
 
       if (videoTitle) {
-        await api.addSongToSheet(videoTitle, youtubeUrl);
+        await api.addSongToSheet(videoTitle, url);
 
         const response = new EmbedBuilder()
           .setTitle("Added a song!")
           .setDescription(`I added "${videoTitle}" to the sheet`);
 
-        await interaction.reply({ embeds: [response] });
+        await interaction.followUp({ embeds: [response] });
       }
     }
   }
 }
+
+// https://open.spotify.com/track/4zfACDG50SflIzKurjd8B0?si=a778fe9dddf24cf2
